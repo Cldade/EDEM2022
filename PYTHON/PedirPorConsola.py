873c4601211e28ecc5b37a68396300e2853ccc57
Claudia Darás <claudiadaras@gmail.com>
2022-09-17 13:59:06 +0200

    Clase Python 170922

---
+++ PYTHON/PedirPorConsola.py
@@ -0,0 +1,6 @@
+name = input("Dime tu nombre: ")
+print(f'Has escrito: {name}')
+
+#Como coger un número cambiando su tipo de datos
+edad = int(input("¿Cuál es tu edad?: "))
+print(f'Hola: {name}. tienes {edad} años')
